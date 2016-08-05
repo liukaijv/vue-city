0572f97734702857a00d7aaa341a42aa39f4c96f
noop <liukaijv@foxmail.com>
2016-08-05 09:29:54 +0800

    Update city-select.vue

--- src/components/city-select.vue
+++ src/components/city-select.vue
@@ -1,13 +1,13 @@
 <template>
-	<select v-model="province" :class="selectClass">
+	<select v-model="province" :class="selectClass" @change="isChanging=true">
 		<option value="">选择省</option>
 		<option v-for="province in provinces" :value="province[valueType]">{{province.name}}</option>
 	</select>
-	<select v-model="city" :class="selectClass">
+	<select v-model="city" :class="selectClass" @change="isChanging=true">
 		<option value="">选择城市</option>
 		<option v-for="city in cities" :value="city[valueType]">{{city.name}}</option>
 	</select>
-	<select v-model="district" :class="selectClass">
+	<select v-model="district" :class="selectClass" @change="isChanging=true">
 		<option value="">选择县/区</option>
 		<option v-for="district in districts" :value="district[valueType]">{{district.name}}</option>
 	</select>
@@ -16,9 +16,12 @@
 
 <script>
 	import citydata  from '../lib/citydata.json';
-	export default {
-		ready(){			
+	export default {		
+		ready(){					
 			this.provinces=citydata;
+			this.oldProvince = this.province;
+			this.oldCity = this.city;
+			this.oldDistrict = this.district;
 			let seletedProvince = this.provinces.filter(function(item){
 				return item[this.valueType] == this.province;
 			}.bind(this));
@@ -34,12 +37,15 @@
 		},
 		props:{	
 			province:{
+				twoWay: true,
 				default:''
 			},
 			city:{
+				twoWay: true,
 				default:''
 			},
 			district:{
+				twoWay: true,
 				default:''
 			},
 			selectClass:{
@@ -52,37 +58,42 @@
 			}
 		},
 		data(){
-			return {	
+			return {
+				oldProvince:'',
+				oldCity:'',
+				oldDistrict:'',	
 				provinces:[],	
 				cities:[],
 				districts:[]			
 			}
 		},	
 		watch:{
-			province(){				
+			province(){					
 				let seletedItem = this.provinces.filter(function(item){
 					return item[this.valueType] == this.province;
 				}.bind(this));			
-				if(seletedItem.length){
+				if(seletedItem.length){					
 					this.cities = seletedItem[0]['children'];
-					this.city = this.city?this.city:this.cities[0][this.valueType];
+					this.city = this.city == this.oldCity ? this.cities[0][this.valueType] : this.city;
 				}else{
 					this.city = '';
 					this.cities = [];
-				}			
+				}		
+				this.oldCity = this.city;	
 				this.$dispatch('provinceChange',seletedItem[0]);			
 			},
-			city(){				
+			city(){	
 				let seletedItem = this.cities.filter(function(item){
 					return item[this.valueType] == this.city;
 				}.bind(this));	
-				if(seletedItem.length){
+				if(seletedItem.length){					
 					this.districts = seletedItem[0]['children'];					
-					this.district = this.district?this.district:this.districts[0][this.valueType];
+					this.district = this.district == this.oldDistrict ? this.districts[0][this.valueType] : this.district;
 				}else{
 					this.district = '';
 					this.districts = [];
-				}				
+				}
+				this.oldDistrict = this.district;				
 				this.$dispatch('cityChange', seletedItem[0]);			
 			},
 			district(){				
@@ -91,6 +102,9 @@
 				}.bind(this));						
 				this.$dispatch('districtChange',seletedItem[0]);
 			}
+		},
+		computed:{
+
 		}
 	}
 </script>
